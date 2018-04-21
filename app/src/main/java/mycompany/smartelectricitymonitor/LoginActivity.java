e9d9ac4995fb2a6ea1b5c36dc12aa104df3760a7
suneth <dse16137@student.nibm.lk>
2018-04-21 15:13:01 +0530

    User Login has been Completed with AsyncTask

--- app/src/main/java/mycompany/smartelectricitymonitor/LoginActivity.java
+++ app/src/main/java/mycompany/smartelectricitymonitor/LoginActivity.java
@@ -1,10 +1,14 @@
 package mycompany.smartelectricitymonitor;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -28,26 +32,47 @@ public class LoginActivity extends AppCompatActivity {
 
     Button btnLogin , btnUserRegistration;
     Intent intent;
+    EditText txtPremisesNo,txtPassword;
+
+    boolean isValidUser = false,loginIsClicked;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.INTERNET}, 0);
+        }
 
         // Initialize
         btnLogin = (Button)findViewById(R.id.btnLogin);
         btnUserRegistration  = (Button)findViewById(R.id.btnUserRegistration);
 
+        txtPremisesNo = (EditText)findViewById(R.id.txtPremisesNo);
+        txtPassword = (EditText)findViewById(R.id.txtPassword);
+
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//
+//                if(txtPremisesNo.getText().equals("")||txtPassword.getText().equals(""))
+//                {
+//                    Toast.makeText(getApplicationContext(),"Please Enter the Credentials",Toast.LENGTH_SHORT).show();
+//                }
+//                else
+//                {
+//
+//                        WebAPITask webAPITask = new WebAPITask();
+//                        webAPITask.execute();
+//
+//                }
 
-                // Redirect to the Domestic User Activity
                 intent = new Intent(LoginActivity.this, IndustrialUserActivity.class);
                 startActivity(intent);
-
 
             }
         });
@@ -65,71 +90,53 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
-
-
     }
 
     private void getUser()
     {
 
-        String getUSerUrl = "https://getfeed.azurewebsites.net/api/Employees";
 
-        Map<String, String> param = new HashMap<String, String>();
-
-//        param.put("emp_ID",txtEmpID.getText().toString());
-//        param.put("emp_Name",txtFname.getText().toString()+" "+txtLname.getText().toString());
-//        param.put("user_Name",txtUName.getText().toString());
-//        param.put("email",txtMail.getText().toString());
-//        param.put("pw",txtPassword.getText().toString());
-//        param.put("employee_tp",txtTelephone.getText().toString());
-//        param.put("department_ID",String.valueOf(deptPosition));
-//        param.put("employee_Type",String.valueOf(empType));
-
+       //String getUSerUrl = "https://ereaderv10.azurewebsites.net/api/Users/P-100/TEST";
+        String getUserUrl = "https://ereaderv10.azurewebsites.net/api/Users/"+txtPremisesNo.getText()+"/"+txtPassword.getText();
 
         try {
 
-            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, getUSerUrl, new JSONObject(param),
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, getUserUrl,null,
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
-                            //Toast.makeText(getApplicationContext(),"Registration Successfully !",Toast.LENGTH_SHORT).show();
-                            //Intent intent = new Intent(EmployeeRegActivity.this, LoginActivity.class);
-                            //startActivity(intent);
+                        try
+                        {
+                           if(response.getString("login").equals("true"))
+                           {
+                               intent = new Intent(LoginActivity.this, IndustrialUserActivity.class);
+                               startActivity(intent);
+                           }
+                           else
+                           {
+                               Toast.makeText(getApplicationContext(),"Please Check the Credentials",Toast.LENGTH_SHORT).show();
+
+                           }
+
+                        }
+                        catch (JSONException ex)
+                        {
+
+                            Toast.makeText(getApplicationContext(), ex.toString(), Toast.LENGTH_SHORT).show();
+
+                        }
 
 
                         }
                     }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(),"Volley Error",Toast.LENGTH_SHORT).show();
                     System.out.println("Error -->>"+error.toString());
-
-
-                }
-            }){
-                @Override
-                protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
-                    try {
-                        String jsonString = new String(response.data,
-                                HttpHeaderParser.parseCharset(response.headers, PROTOCOL_CHARSET));
-
-                        JSONObject result = null;
-
-                        if (jsonString != null && jsonString.length() > 0)
-                            result = new JSONObject(jsonString);
-
-                        return Response.success(result,
-                                HttpHeaderParser.parseCacheHeaders(response));
-                    } catch (UnsupportedEncodingException e) {
-                        return Response.error(new ParseError(e));
-                    } catch (JSONException je) {
-                        return Response.error(new ParseError(je));
-                    }
-
+                    isValidUser = false;
 
                 }
-            };
+            });
 
             request.setRetryPolicy(new
 
@@ -143,9 +150,32 @@ public class LoginActivity extends AppCompatActivity {
         }
         catch (Exception ex)
         {
+
             System.out.print("Exception"+ex.toString());
 
         }
 
     }
+
+
+    private class WebAPITask extends AsyncTask<Void,Void,Void>
+    {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try
+            {
+               getUser();
+            }
+            catch (Exception ex)
+            {
+                Toast.makeText(getApplicationContext(),"Please turn on the Mobile Data",Toast.LENGTH_SHORT).show();
+            }
+            return null;
+
+        }
+
+    }
+
+
 }
